f0c1b459b8e3f2526a9ef7ad442fd685eaa12f22
Maximiliano <maxiorellana555@gmail.com>
2025-06-17 23:21:12 -0300

    feat: freq table
    cositas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'; // Import the Header component
 import Footer from './components/Footer'; // Import the Footer component
 import InputView from './components/InputView';
+import FreqTable from './components/FreqTable';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <Header />
       <InputView />
+      <FreqTable />
       <Footer />
     </div>
   );
--- frontend/src/components/FreqTable.jsx
+++ frontend/src/components/FreqTable.jsx
@@ -1,13 +1,84 @@
 import React from "react";
 import './FreqTable.css';
-import { Chart, defaults } from "chart.js";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+  },
+  scales: {
+    x: {
+      categoryPercentage: 0.95, // default es 0.8
+      barPercentage: 0.95,      // default es 0.9
+      grid: {
+        display: false          // para sacar la cuadrícula
+      }
+    },
+    y: {
+      grid: {
+        display: false          // sacar la cuadrícula del eje y también
+      }
+    }
+  }
+};
+
+const DATOS = [
+  {
+    word: 'p',
+    freq: 15,
+  },
+  {
+    word: 'a',
+    freq: 23,
+  },
+  {
+    word: 'b',
+    freq: 21
+  }
+];
+
+export const data = {
+  labels: DATOS.map(e => e.word),
+  datasets: [
+    {
+      label: 'Frecuencia de Bytes',
+      data: DATOS.map(e => e.freq),
+      backgroundColor: 'rgba(43, 54, 116, 1)',
+      borderWidth: 0,
+      barThickness: 20,
+      borderRadius: 20,
+      borderSkiped: false,
+    }
+  ]
+};
 
 const FreqTable = () => {
-
-
   return (
-  <>
-  </>
+    <div id='freq-table'>
+      <Bar options={options} data={data}  />
+    </div>
   );
 }
 
