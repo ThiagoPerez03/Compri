f916d1e95a3a3c3eb373a5d406f9711ce4989598
ThiagoDanielPerez <thiagodanielperez@alu.frlp.utn.edu.ar>
2025-06-20 17:37:53 -0300

    cambios 2

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('compress/', views.api_de_compresion, name='api_de_compresion'),
-    path('decompress/', views.api_de_descompresion, name='api_de_descompresion'),
+    # Se añade 'api/' para que la ruta completa sea /app/api/compress/
+    path('api/compress/', views.api_de_compresion, name='api_de_compresion'),
+    
+    # Se añade 'api/' para que la ruta completa sea /app/api/decompress/
+    path('api/decompress/', views.api_de_descompresion, name='api_de_descompresion'),
 ]
